e807532e22961f1c9521345160ed3e3fd9acc3da
Selftishz <art2sys048@gmail.com>
2022-12-26 19:00:09 +0300

    dd

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Node {
+public class Node {
+    private int data;
+    private Node leftChild;
+    private Node rightChild;
+
+    public Node(int newData) {
+        data = newData;
+    }
+
+    public void setLeftChild(Node newNode) {
+        leftChild = newNode;
+    }
+
+    public void setRightChild(Node newNode) {
+        rightChild = newNode;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public int getData() {
+        return data;
+    }
 }
